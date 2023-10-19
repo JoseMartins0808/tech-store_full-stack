1ebd68c5c5f4a4ab752e669928f3bea53f2933af
JoseMartins0808 <ottenazuose@hotmail.com>
2023-10-18 22:39:24 -0300

    feat: first banner for home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,14 +1,17 @@
 "use client";
 
 import { useSession } from "next-auth/react";
+import Image from "next/image";
+
+
 export default function Home() {
 
   const { data } = useSession();
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h2>Nome do usuário: {data?.user?.name}</h2>
-      <h3>Email do usuário: {data?.user?.email}</h3>
+      <Image src="/banner-home-01.png" alt="Until 55% off sale just this month" height={0} width={0}
+        className="h-auto w-full" sizes="100vw" />
     </main>
   )
 }
